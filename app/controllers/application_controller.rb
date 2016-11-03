29641fda455bcac6d117f44710ef95dbbd713cc5
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-03 16:28:33 -0400

    Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+  def authenticate
+    render json: {status: 401, message: "unauthorized"} unless decode_token(bearer_token)
+  end
+
+  def bearer_token
+    pattern = /^Bearer /
+    header  = request.env["HTTP_AUTHORIZATION"] # <= env
+    header.gsub(pattern, '') if header && header.match(pattern)
+  end
+
+  def current_user
+    return if !bearer_token
+    decoded_jwt = decode_token(request.env["HTTP_AUTHORIZATION"])
+
+    User.find(decoded_jwt.user.id)
+  end
+
+  def decode_token(token)
+    token = JWT.decode(token, nil, false) # this will error if the token is invalid or expired
+  rescue
+    render json: {status: 401, message: 'invalid or expired token'}
+  end
+
 end

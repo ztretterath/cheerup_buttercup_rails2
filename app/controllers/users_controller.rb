130045841177376e5c2b16de71ec5b3b65d65baf
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-03 12:01:09 -0400

    Users controller created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,44 @@ class UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+  # GET /users/1
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  #POST/users
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+
+    render json: {status: 204}
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:last_name, :first_name, :username, :password, :email)
+    end
 end
- 

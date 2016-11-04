f4db2991f6f254746160ea6aaff97db12ab51ccc
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-04 15:47:00 -0400

    Added add_review and update_review

--- app/controllers/cheer_ups_controller.rb
+++ app/controllers/cheer_ups_controller.rb
@@ -11,9 +11,36 @@ class CheerUpsController < ApplicationController
     render json: {status: 200, cheer_up: cheer_up}
   end
 
+  # Adds a review to a cheerup
+  def add_review
+    cheer_up = CheerUp.find(params[:id])
+    cheer_up.reviews.create(review_params)
+
+    render json:{
+      status: 200,
+      cheer_up: cheer_up,
+      review: cheer_up.reviews
+    }
+  end
+
+  # Updates a cheerup review
+
+  def update_review
+    review = Review.find(params[:id])
+
+    review.update(review_params)
+
+    render json: {status: 200, review: review}
+  end
+
+
   private
     def cheer_up_params
         params.require(:cheer_up).permit(:title, :content, :category, :user_id)
+    end
+
+    def review_params
+      params.require(:review).permit(:value, :cheer_up_id, :user_id)
     end
 
 end

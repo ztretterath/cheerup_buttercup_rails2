1984d9a7a505da0b3d69170f08722dfb579a1fd9
Jason Fry <jmphry@gmail.com>
2016-11-04 18:39:30 -0400

    Prevent a user from creating multiple reviews on a cheer_up

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ApplicationRecord
+  validates :user_id, :uniqueness =>
+    {
+      :scope => :cheer_up_id,
+      :message => "Users may only write one review per CheerUp."
+    }
   belongs_to :cheer_up
   belongs_to :user
 end

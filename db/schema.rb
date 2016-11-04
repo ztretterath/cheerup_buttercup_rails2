878009a3d491b52dc87b3e41afe4812e8adb0fd9
Zarela Velasquez <zarelavp@yahoo.com>
2016-11-03 21:05:26 -0400

    Authentication working FINALLY! XD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103144831) do
+ActiveRecord::Schema.define(version: 20161104003627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(version: 20161103144831) do
     t.string   "username"
     t.string   "password"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   add_foreign_key "cheer_ups", "users"

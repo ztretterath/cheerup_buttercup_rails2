2619027ff9b6f16ec171f177317a123dc22b87eb
Jason Fry <jmphry@gmail.com>
2016-11-06 22:16:42 -0500

    Change relationships that prevented a user from deleting their account
    Now a user is able to delete their account, but it will also delete all of their cheerups and reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  has_many :cheer_ups
-  has_many :reviews
+  # Had to add `dependent: :destroy` to these relationships to allow the user to
+  # be deleted.  We do not want this.  We want a user's cheerups and reviews to
+  # persist even if the user is deleted.  This may not be possible on a
+  # relational database, and we were unable to find a solution.
+  has_many :cheer_ups, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   # validates :username, presence: true, uniqueness: true
   has_secure_password
 end
